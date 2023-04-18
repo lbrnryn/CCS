4dcf5946c9860ec15aca757f7cfb147228ba50ec
lbrnryn <lbrnryn@yahoo.com>
2023-04-18 21:10:39 +0800

    Add readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+# ICCT CCS Tech Guild
+
+A dynamic website for my school's programming club that enables students to easily reserve seats for live coding tutorials and receive announcements. The website serves as a central hub for the club to promote programming throughout the school community and foster a stronger network of developers.
--- index.js
+++ index.js
@@ -145,12 +145,25 @@ app.post("/login", (req, res, next) => {
     })(req, res, next);
 });
 
+app.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) { return next(err) }
+    });
+    res.redirect("/");
+})
+// app.post("/logout", (req, res, next) => {
+//     req.logout((err) => {
+//         if (err) { return next(err) }
+//     });
+//     res.redirect("/");
+// })
 app.delete("/logout", (req, res, next) => {
     req.logOut((err) => {
         if (err) { return next(err) }
     });
     res.redirect("/");
 });
+
 
 app.get("/profile", checkAuthenticated, async (req, res) => {
     try {
